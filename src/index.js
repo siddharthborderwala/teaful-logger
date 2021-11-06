35ce5ae418cba17c1585b06d9c724cd754176e09
Siddharth Borderwala <siddharthborderwala@gmail.com>
2021-11-06 23:08:07 +0530

    upgrades

--- src/index.js
+++ src/index.js
@@ -1,6 +1,53 @@
-const logger = ({ store, prevStore }) => {
-  console.log("%cOld Store:", "color: #F09479; font-weight: 700;", prevStore);
-  console.log("%cUpdated Store:", "color: #73D673; font-weight: 700;", store);
+const format = (time) => time.toString().padStart(2, '0');
+
+/**
+ * Logger utility function
+ *
+ * @param {{ prevStore: Object, store: Object }} storeValues The old and updated value of the store
+ * @param {string} name Name of the logger
+ */
+export const logger = (storeValues, name) => {
+  const date = new Date();
+  const hrs = format(date.getHours());
+  const min = format(date.getMinutes());
+  const sec = format(date.getSeconds());
+  const ms = date.getMilliseconds();
+  if (name) {
+    console.log(
+      `%cUpdate: ${name} %c@ ${hrs}:${min}:${sec}.${ms}`,
+      'color: #EC967D; font-weight: 700;',
+      'color: #A9C0C9;'
+    );
+  } else {
+    console.log(
+      `%cUpdate %c@ ${hrs}:${min}:${sec}.${ms}`,
+      'color: #EC967D; font-weight: 700;',
+      'color: #A9C0C9;'
+    );
+  }
+  console.log(
+    '%cOld Store:',
+    'color: #A0CFEF; font-weight: 700;',
+    storeValues.prevStore
+  );
+  console.log(
+    '%cUpdated Store:',
+    'color: #B0F39F; font-weight: 700;',
+    storeValues.store
+  );
 };
 
-export default logger;
+/**
+ * Creates and returns a named logger utility
+ *
+ * @param {string} name Name of the logger
+ * @returns {Function} Logger function with the specified name
+ */
+export const namedLogger = (name) => {
+  if (!name) {
+    throw new Error('Named logger requires a name (of type string)');
+  }
+  return (storeValues) => {
+    logger(storeValues, name);
+  };
+};
